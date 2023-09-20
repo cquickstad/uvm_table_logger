5e7561dedf7c8ebc624e00150fc9ab62c53f8607
Chad Quickstad <chad@andestech.com>
2023-09-19 17:40:36 -0700

    Create a table_logger_pkg package for better use of the table logger across multiple Agents and VIPs.

---
+++ table_logger_pkg.sv
@@ -0,0 +1,16 @@
+`ifndef __TABLE_LOGGER_PKG_SV__
+`define __TABLE_LOGGER_PKG_SV__
+
+package table_logger_pkg;
+
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  `include "txt_tbl.sv"
+  `include "enum_width_finder.svh"
+  `include "table_logger_cfg.svh"
+  `include "table_logger.svh"
+
+endpackage
+
+`endif
